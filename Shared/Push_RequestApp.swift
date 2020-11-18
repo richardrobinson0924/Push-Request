eec6fa3791f1d063b95d4f214c08ed55539e8646
Richard Robinson <rir@my.yorku.ca>
2020-11-17 22:34:24 -0500

    Initial Commit

---
+++ Shared/Push_RequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_RequestApp.swift
+//  Shared
+//
+//  Created by Richard Robinson on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Push_RequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
